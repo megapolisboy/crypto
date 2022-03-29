4cfc3cb4b0417a347efea59d0742a486649813fc
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-03-29 14:18:34 +0300

    Add antdesign layout, create navbar

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  Homepage,
+  Navbar,
+  News,
+} from "./components";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+};
+
+export default App;
