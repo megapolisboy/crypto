6c54b70bf3e95c2735b39e25cbaa1a54a4033e2c
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-03-29 14:42:29 +0300

    layout fully created, router added

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,5 +1,36 @@
+import { Col, Row, Statistic, Typography } from "antd";
+import millify from "millify";
+
+const { Title } = Typography;
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total crypto currencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total crypto currencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total crypto currencies" value="5" />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Homepage;
